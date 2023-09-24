1387576e1af35260c732938645833a4d7d17ba3e
Maxi0703 <maxi07032001@gmail.com>
2023-09-23 23:02:05 -0300

    commit_toast_validaciones

--- src/app/auth/olvidar-contrasena/olvidar-contrasena.page.ts
+++ src/app/auth/olvidar-contrasena/olvidar-contrasena.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-olvidar-contrasena',
@@ -7,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OlvidarContrasenaPage implements OnInit {
 
-  constructor() { }
+  email: String = '';
+
+  emailSimulado = 'alumno@duoc.cl'; 
+  constructor(private toastController: ToastController,
+              private principal: NavController) { }
+
+  async recuperar() {
+    if (this.email === this.emailSimulado) {
+      this.principal.navigateForward('/home');
+  
+      const toast = await this.toastController.create({
+        message: 'Se ha enviado la solicitud a su correo!',
+        duration: 3000,
+        position: 'bottom',
+        color: 'success'
+      });
+      await toast.present();
+  
+    }
+  
+    else{
+  
+      const toast = await this.toastController.create({
+        message: 'Email no válido!',
+        duration: 3000,
+        position: 'bottom',
+        color: 'danger'
+      });
+      await toast.present();
+    }
+      
+    }
 
   ngOnInit() {
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,13 +15,34 @@ contrasena: string = '';
 alumnoSimulado = 'alumno';
 contrasenaAlumnoSimulada = 'contrasena';
 
-constructor(private principal: NavController) {}
+constructor(private principal: NavController,
+            private toastController: ToastController) {}
 
-login() {
+async login() {
   if (this.usuario === this.alumnoSimulado && this.contrasena === this.contrasenaAlumnoSimulada) {
     this.principal.navigateForward('/alumno-inicio');
-  }
-    console.log('Credenciales incorrectas');
+
+    const toast = await this.toastController.create({
+      message: 'Ha ingresado exitosamente!',
+      duration: 3000,
+      position: 'bottom',
+      color: 'success'
+    });
+    await toast.present();
+
   }
 
-}
+  else{
+
+    const toast = await this.toastController.create({
+      message: 'Credenciales no válidas!',
+      duration: 3000,
+      position: 'bottom',
+      color: 'danger'
+    });
+    await toast.present();
+  }
+    
+  }
+
+}//clase
